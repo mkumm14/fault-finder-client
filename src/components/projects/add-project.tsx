9b9f6e238aa127c0780d82856c2498c788fd21e8
mkumm14 <madhavkummara@outlook.com>
2023-09-06 19:41:16 -0500

    settting up form schema for add project

--- src/components/projects/add-project.tsx
+++ src/components/projects/add-project.tsx
@@ -1,5 +1,6 @@
 import {Button} from "@/components/ui/button";
 import {Icons} from "@/components/icons";
+
 import {
     Sheet,
     SheetClose,
@@ -11,18 +12,41 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet";
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+
+
 import { Textarea } from "@/components/ui/textarea"
 
 import {Label} from "@/components/ui/label"
 
 import {Input} from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  })
 
 export default function AddProject() {
 
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          username: "",
+        },
+      })
+
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
 
     return (
         <div className="flex justify-end mb-4">
-            <Sheet>
+            <Sheet> 
                 <SheetTrigger asChild>
 
                     <Button variant="ghost" size="sm" className="h-8 w-8 px-0"
@@ -30,7 +54,7 @@ export default function AddProject() {
                         <Icons.add/>
                     </Button>
                 </SheetTrigger>
-                <SheetContent className="w-[400px] md:w-[640px] overflow-auto">
+                <SheetContent className="md:w-1/2 w-full overflow-auto">
                     <SheetHeader>
                         <SheetTitle>Add Project</SheetTitle>
                         <SheetDescription>
@@ -50,12 +74,7 @@ export default function AddProject() {
                             </Label>
                             <Textarea id='description' />
                         </div>
-                        <div className="grid  gap-4">
-                            <Label htmlFor="description" >
-                                Description
-                            </Label>
-                            <Textarea id='description' />
-                        </div>
+        
                     </div>
                     <SheetFooter>
                         <SheetClose asChild>
