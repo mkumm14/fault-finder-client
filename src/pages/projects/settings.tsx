06cc24d254d398a4297bc54117bec4fcab183cf5
mkumm14 <madhavkummara@outlook.com>
2024-01-22 18:16:17 -0600

    add project details to project settings page

--- src/pages/projects/settings.tsx
+++ src/pages/projects/settings.tsx
@@ -1,6 +1,9 @@
 import { useRetrieveProjectDetailsQuery } from "@/features/project-api-slice";
 import { useParams } from "react-router-dom"
 
+
+
+
 export default function ProjectSettings() {
 
     const { projectId } = useParams()
@@ -19,9 +22,20 @@ export default function ProjectSettings() {
 
     return (
         <div>
-            <p className="text-4xl">{project.title}</p>
-            
+            <p>Project Settings</p>
+            <div className=" pt-5">
 
+                <p>{project.title}</p>
+                <p>{project.description}</p>
+                <p>{project.users}</p>
+                <p>{new Date(project.created_date).toDateString()}</p>
+                <p>{new Date(project.updated_date).toDateString()}</p>
+                <p>{project.owner_username}</p>
+                <p>{project.updated_by_username}</p>
+
+
+
+            </div>
         </div>
     )
 
