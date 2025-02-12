e1e3941e3288b882a9b9050755db358e96d734da
mkumm14 <madhavkummara@gmail.com>
2025-02-11 20:37:59 -0600

    fix tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    outDir: "build", 
+  },
   plugins: [react()],
   resolve: {
     alias: {
