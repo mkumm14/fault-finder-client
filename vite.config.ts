40c07c314622d5885b8c80b75ceb78603c25b2d3
mkumm14 <madhavkummara@gmail.com>
2025-02-11 21:07:09 -0600

    add outDir

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    outDir: "dist", // ✅ Ensure this is "dist"
+  },
   plugins: [react()],
   resolve: {
     alias: {
